15e87a59403c92ed065a775837a64d5e77ecefc7
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-11-07 21:49:13 +0200

    new

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 
 export default function Calculator(props) {
     let [firstNumber, setFirstNumber] = useState('');
-    let [secondNumber, setSecondNumber] = useState('');
+    // let [secondNumber, setSecondNumber] = useState('');
     let first = 0.80;
     let second = 0.90;
     let third = 0.95;
     let [unit, setUnit] = useState(`${first}`);
     let usd = props.info;
+    let [moneyUpwork, setmoneyUpwork] = useState('');
+    let [moneyPayoneer, setmoneyPayoneer] = useState('');
+    let [moneyUah, setMoneyUah] = useState('');
 
     function calculatingNumber() {
-        let x = firstNumber * unit;
-        let y = (x - 1) * 0.98 * usd;
-        setSecondNumber(Math.round(y));
-        
-        // setSecondNumber( ((firstNumber * unit) - 1) * 0.98 * usd );
+        setmoneyUpwork(firstNumber * unit);
+        setmoneyPayoneer(moneyUpwork - 1);
+        setMoneyUah(Math.round(moneyPayoneer * 0.98 * usd));
     }
+
     function changeUnit(event) {
         event.preventDefault();
         setFirstNumber('');
@@ -28,21 +30,30 @@ export default function Calculator(props) {
                 <legend>Чи добре ти знайом з клієнтом?</legend>
                 <div>
                     <select name="first unit" value={unit} onChange={changeUnit}>
-                        <option value={first}>Ні - заробив з ним менше 500 гривень</option>
-                        <option value={second}>Так - заробив з ним більше 500 гривень</option>
+                        <option value={first}>Ні - заробив з ним менше 500 $</option>
+                        <option value={second}>Так - заробив з ним більше 500 $</option>
                         <option value={third}>Заробив шалену купу грошей</option>
                     </select>
                 </div>
                 <div>
-                    <input onChange={event => { setSecondNumber(''); setFirstNumber(event.target.value); }} value={firstNumber} />
+                    <input type="text" onChange={event => { setmoneyUpwork(''); setmoneyPayoneer(''); setMoneyUah(''); setFirstNumber(event.target.value); }} value={firstNumber} />
+                </div>
+                <div>
+                    <input type = "submit" value="Порахувати" onSubmit={event => { event.preventDefault(); calculatingNumber(); }}/>
                 </div>
             </form>
             <p className="hi">
                 Вітаю, Дмитро!
             </p>
             <p className="number">
-                Ти заробив: <p className="hi">{secondNumber}</p> грн.  
+                Ти заробив на UpWork: <p className="hi">{moneyUpwork} $</p>  
             </p>
+            <p className="number">
+                На Payoneer ти отримаєш: <p className="hi">{moneyPayoneer} $</p> 
+            </p>
+            <p className="number">
+                На PrivatBank тобі прийде: <p className="hi">{moneyUah} ₴</p> 
+            </p>            
         </div>
     );
 }
